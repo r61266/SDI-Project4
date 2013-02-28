ec13c257515159df93bb27795208010cd9a4e065
r61266 <lightking@fullsail.edu>
2013-02-28 05:24:14 -0500

    added information to javascript and modified html to link up with the .js file.

--- Shallenberger_Alec_assignment4.js
+++ Shallenberger_Alec_assignment4.js
@@ -1,1 +1,30 @@
 alert("JavaScript works!");
+
+// Alec Shallenberger
+// February 28, 2013
+// Term 1302
+// Project4
+
+//String using a phone number
+var PhoneNumber = "561-934-4967";
+var end = PhoneNumber.indexOf("-");
+var start = PhoneNumber.indexOf("-")+1;
+
+console.log(PhoneNumber.charAt(6));
+console.log(PhoneNumber.indexOf("5"));
+console.log(PhoneNumber.lastIndexOf("7"));
+console.log(PhoneNumber.substring(0,end));
+
+//String using Email Address
+var Email = "lightking@fullsail.edu" 
+var start = Email.indexOf("l")
+var end = Email.lastIndexOf("u")
+
+console.log(Email.charAt(6));
+console.log(Email.substring(start,end));
+
+// Array Functions
+
+
+		
+	
